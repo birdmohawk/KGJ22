b8d68ea21b110ca6d9201ef4d453ea5ad5852914
Nathaniel <nat_pennington@hotmail.co.nz>
2022-07-23 15:42:44 +1200

    Latest push, UI work and movement

---
+++ Coffee Coffee Coffee/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public float Coffee_temprature = 10;
+    public float Coffee_beans = 10;
+    public float Coffee_size = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
