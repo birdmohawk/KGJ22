3d7d112a333f2895f1912a13dcab9aeca757eb46
Nathaniel <nat_pennington@hotmail.co.nz>
2022-07-23 14:48:19 +1200

    Cinemachine

--- Coffee Coffee Coffee/Assets/Scripts/CameraMovement.cs
+++ Coffee Coffee Coffee/Assets/Scripts/CameraMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public GameObject player;
+    public float camOffset = -10;
+    public float camSmoothSpeed = 10;
+    public float TickTime = 2;
+
+    float TickTimer = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        TickTimer += Time.deltaTime;
+
+        if(TickTimer > TickTime)
+        {
+            Debug.Log(Vector3.Distance(transform.position, player.transform.position));
+            if(Vector3.Distance(transform.position , player.transform.position) > 5)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, camOffset), camSmoothSpeed);
+            }
+            else
+            {
+                TickTimer = 0f;
+            }
+
+        }
     }
 }
