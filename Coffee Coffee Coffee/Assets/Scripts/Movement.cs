d253cce5a768249e2ab9005ddd39d6c519033c57
Nathaniel <nat_pennington@hotmail.co.nz>
2022-07-23 14:03:12 +1200

    Push the background and movement for the player

---
+++ Coffee Coffee Coffee/Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpPower = 1f;
+    Rigidbody2D rb;
+
+    public bool isGrounded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(movement * moveSpeed, rb.velocity.y);
+
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * jumpPower * 10);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Hit Ground");
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Leave Ground");
+            isGrounded = false;
+        }
+    }
+
+}
